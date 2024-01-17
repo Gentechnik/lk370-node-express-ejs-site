c12ab405a5c861fb8016984e22d1915f57bfb942
Lukas Karberg <deaderlp@googlemail.com>
2024-01-17 09:51:27 +0100

    Add dependencies and devDependencies to package.json

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app = express();
+const port = 4112;
+
+app.get("/", (req, res) => {
+  res.send(`
+    <html>
+    <head>
+    <title>Info Site</title></head>
+    <body>
+        <h1>Info Site</h1>
+        <p>This is an Info Site</p>
+    </body></html>`);
+});
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
